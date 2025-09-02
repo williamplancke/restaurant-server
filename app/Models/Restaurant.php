cc4797ebc8d4e506325061694417d181e2888bad
william.plancke <william.plancke@student.howest.be>
2025-09-02 11:17:32 +0200

    Seed database

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Restaurant extends Model
 {
-    use HasUuids;
+    use HasUuids, HasFactory;
     function Bookings():HasMany {
         return $this->hasMany(Booking::class);
     }
